a39eb2edacb3780356d8323c1a25cea77dd0d8ab
josh-reynolds <josh.reynolds@frontier.com>
2024-01-10 14:29:51 -0800

    Chapter 03: Cons the Magnificent - through p. 37

--- Chapter_03/definitions.rkt
+++ Chapter_03/definitions.rkt
@@ -9,3 +9,24 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 ; ------------------------------
+
+; ------------------------------
+(define rember
+  (lambda (a lat)
+    (cond
+      ((null? lat) '())
+      ((eq? a (car lat)) (cdr lat))
+      (else (cons (car lat)
+                  (rember a
+                          (cdr lat)))))))
+; ------------------------------
+
+
+(define list1
+  (list 'lamb 'chops 'and 'mint 'jelly))
+
+(define list2
+  (list 'coffee 'cup 'tea 'cup 'and 'hick 'cup))
+
+(define list3
+  (list 'bacon 'lettuce 'and 'tomato))
