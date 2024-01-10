b24c945ede4c69d9ad566be4b626a300d43b4e43
josh-reynolds <josh.reynolds@frontier.com>
2024-01-10 14:00:47 -0800

    Chapter 02: Do It, Do It Again, and Again, and Again... - complete

--- Chapter_02/definitions.rkt
+++ Chapter_02/definitions.rkt
@@ -2,9 +2,47 @@
 ; ------------------------------
 ; The Little Schemer (Fourth Edition)
 ; Chapter 2: Do It, Do It Again, and Again, and Again...
+; ------------------------------
 
 ; ------------------------------
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 ; ------------------------------
+
+; ------------------------------
+(define lat?
+  (lambda (l)
+    (cond
+      ((null? l) #t)
+      ((atom? (car l)) (lat? (cdr l)))
+      (else #f))))
+; ------------------------------
+
+; ------------------------------
+(define member?
+  (lambda (a lat)
+    (cond
+      ((null? lat) #f)
+      (else (or (eq? (car lat) a)
+                (member? a (cdr lat)))))))
+; ------------------------------
+
+(define list1
+  (list 'Jack 'Sprat 'could 'eat 'no 'chicken 'fat))
+
+(define list2
+  (list 'Jack (list 'Sprat 'could) 'eat 'no 'chicken 'fat))
+
+(define list3
+  (list 'bacon 'and 'eggs))
+
+(define list4
+  (list 'bacon
+        (list 'and 'eggs)))
+
+(define list5
+  (list 'mashed 'potatoes 'and 'meat 'gravy))
+
+(define list6
+  (list 'bagels 'and 'lox))
