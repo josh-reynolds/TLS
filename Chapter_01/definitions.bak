e8b8573a837c8402d02ccc792ce9b1de2ffb43b3
josh-reynolds <josh.reynolds@frontier.com>
2024-01-09 16:15:10 -0800

    Chapter 01: Toys - through p. 6

---
+++ Chapter_01/definitions.bak
@@ -0,0 +1,9 @@
+#lang r5rs
+
+(define atom?
+  (lambda (x)
+    (and (not (pair? x)) (not (null? x)))))
+
+
+
+
--- Chapter_01/definitions.rkt
+++ Chapter_01/definitions.rkt
@@ -1,9 +1,33 @@
 #lang r5rs
+; ------------------------------
+; The Little Schemer (Fourth Edition)
+; Chapter 1: Toys
 
+; ------------------------------
 (define atom?
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
+; ------------------------------
 
+(define la
+  (list 'a 'b 'c))
 
+(define lb
+  (list la 'x 'y 'z))
 
+(define lc
+  (list
+   (list
+    (list 'hotdogs))
+   (list 'and)
+   (list 'pickle)
+   'relish))
 
+(define ld
+  (list
+   (list
+    (list 'hotdogs))
+   (list 'and)))
+
+(define le
+  (list 'hamburger))
