2443b581066e0d2b75676a24bb570bd143beaa07
josh-reynolds <josh.reynolds@frontier.com>
2024-01-13 22:59:26 -0800

    Chapter 04: Numbers Games - through p. 73

--- Chapter_04/definitions.rkt
+++ Chapter_04/definitions.rkt
@@ -62,3 +62,30 @@
       ((null? tup2) tup1)
       (else (cons (o+ (car tup1)(car tup2))(tup+ (cdr tup1)(cdr tup2)))))))
 ; ------------------------------
+
+; ------------------------------
+(define gt?                   ; text uses '>' but this is already a defined primitive operation
+  (lambda (n m)
+    (cond
+      ((zero? n) #f)
+      ((zero? m) #t)
+      (else (gt? (sub1 n) (sub1 m))))))
+; ------------------------------
+
+; ------------------------------
+(define lt?                   ; text uses '<' but this is already a defined primitive operation
+  (lambda (n m)
+    (cond
+      ((zero? m) #f)
+      ((zero? n) #t)
+      (else (lt? (sub1 n) (sub1 m))))))
+; ------------------------------
+
+; ------------------------------
+(define equals?               ; text uses '=' but this is already a defined primitive operation
+  (lambda (n m)               ; and the shorter 'eq?' is also already defined
+    (cond
+      ((lt? n m) #f)
+      ((gt? n m) #f)
+      (else #t))))
+; ------------------------------
