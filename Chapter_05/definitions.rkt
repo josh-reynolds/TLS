39ae2d82b5ab1e2ba3f31f033f25008e0e8a05d4
josh-reynolds <josh.reynolds@frontier.com>
2024-01-16 14:14:34 -0800

    Chapter 05: *Oh My Gawd*: It's Full of Stars - through p. 92

--- Chapter_05/definitions.rkt
+++ Chapter_05/definitions.rkt
@@ -135,18 +135,23 @@
   (lambda (l1 l2)
     (cond
       ((and (null? l1)(null? l2)) #t)
-      ((and (null? l1)(atom? (car l2))) #f)
-      ((null? l1) #f)
+      ((or (null? l1)(null? l2)) #f)
       ;
-      ((and (atom? (car l1))(null? l2) #f))
       ((and (atom? (car l1))(atom? (car l2))) (and (equan? (car l1)(car l2))
                                                    (eqlist? (cdr l1)(cdr l2))))
-      ((atom? (car l1)) #f)
+      ((or (atom? (car l1))(atom? (car l2))) #f)
       ;
-      ((null? l2) #f)
-      ((atom? (car l2)) #f)
       (else (and (eqlist? (car l1)(car l2))
                  (eqlist? (cdr l1)(cdr l2)))))))
+; ------------------------------
+
+; ------------------------------
+(define o-equal?              ; the text uses 'equal?' but this (and 'eq?)
+  (lambda (s1 s2)             ; is already defined
+    (cond
+      ((and (atom? s1)(atom? s2)) (equan? s1 s2))
+      ((or (atom? s1)(atom? s2)) #f)
+      (else (eqlist? s1 s2)))))
 ; ------------------------------
 
 (define list1
