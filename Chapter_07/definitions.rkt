5e906b0d2a78ee6e6acf8d6f4864aa56d1535b8a
josh-reynolds <josh.reynolds@frontier.com>
2024-01-18 14:33:02 -0800

    Chapter 07: Friends and Relations - through p. 118

--- Chapter_07/definitions.rkt
+++ Chapter_07/definitions.rkt
@@ -161,6 +161,17 @@
                        (intersectall (cdr l-set)))))))
 ; ------------------------------
 
+; ------------------------------
+(define a-pair?
+  (lambda (x)
+    (cond
+      ((atom? x) #f)
+      ((null? x) #f)
+      ((null? (cdr x)) #f)
+      ((null? (cdr (cdr x))) #t)
+      (else #f))))
+; ------------------------------
+
 (define list1
   (list 'apple 'peaches 'apple 'plum))
 
