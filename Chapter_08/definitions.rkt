a7d9d44dc40ac78abb0b511a9f737ee3a8bfe1d7
josh-reynolds <josh.reynolds@frontier.com>
2024-01-19 12:47:39 -0800

    Chapter 08: Lambda the Ultimate - through p. 126

--- Chapter_08/definitions.rkt
+++ Chapter_08/definitions.rkt
@@ -9,3 +9,26 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 ; ------------------------------
+
+; ------------------------------
+(define rember-f
+  (lambda (test? a l)
+    (cond
+      ((null? l) '())
+      ((test? a (car l)) (cdr l))
+      (else (cons (car l)
+                  (rember-f test? a (cdr l)))))))
+; ------------------------------
+
+
+(define list1
+  (list 6 2 5 3))
+
+(define list2
+  (list 'jelly 'beans 'are 'good))
+
+(define list3
+  (list 'lemonade
+        (list 'pop 'corn)
+        'and
+        (list 'cake)))
