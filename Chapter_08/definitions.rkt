05c1eca06561a9c905a240a273bfe0b729bade52
josh-reynolds <josh.reynolds@frontier.com>
2024-01-19 14:14:25 -0800

    Chapter 08: Lambda the Ultimate - through p. 129

--- Chapter_08/definitions.rkt
+++ Chapter_08/definitions.rkt
@@ -20,6 +20,33 @@
                   (rember-f test? a (cdr l)))))))
 ; ------------------------------
 
+; ------------------------------
+(define eq?-c
+  (lambda (a)
+    (lambda (x)
+      (eq? x a))))
+; ------------------------------
+
+; ------------------------------
+(define eq?-salad
+  (eq?-c 'salad))
+; ------------------------------
+
+; ------------------------------
+(define rember-f2             ; rewriting to use curried function
+  (lambda (test?)             ; text overwrites but I want to preserve the original
+    (lambda (a l)
+      (cond
+        ((null? l) '())
+        ((test? a (car l)) (cdr l))
+        (else (cons (car l)
+                    ((rember-f2 test?) a (cdr l))))))))
+; ------------------------------
+
+; ------------------------------
+(define rember-eq? (rember-f2 eq?))
+; ------------------------------
+
 
 (define list1
   (list 6 2 5 3))
@@ -32,3 +59,9 @@
         (list 'pop 'corn)
         'and
         (list 'cake)))
+
+(define list4
+  (list 'tuna 'salad 'is 'good))
+
+(define list5
+  (list 'shrimp 'salad 'and 'tuna 'salad))
