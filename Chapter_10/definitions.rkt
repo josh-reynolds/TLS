cd50c26bdbcc79a72e37f349652ac18bf32544d6
josh-reynolds <josh.reynolds@frontier.com>
2024-01-25 13:10:28 -0800

    Chapter 10: What is the Value of All of This? - through p. 175

--- Chapter_10/definitions.rkt
+++ Chapter_10/definitions.rkt
@@ -9,3 +9,33 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 ; ------------------------------
+
+; ------------------------------
+(define build
+  (lambda (s1 s2)
+    (cons s1 (cons s2 '()))))
+; ------------------------------
+
+; ------------------------------
+(define new-entry build)
+; ------------------------------
+
+; ------------------------------
+(define lookup-in-entry
+  (lambda (name entry)
+    (...)))
+; ------------------------------
+
+
+(define entry1
+  (list (list 'appetizer 'entrée 'beverage)
+        (list 'paté 'boeuf 'vin)))
+
+(define entry2
+  (list (list 'appetizer 'entrée 'beverage)
+        (list 'beer 'beer 'beer)))
+
+(define entry3
+  (list (list 'beverage 'dessert)
+        (list (list 'food 'is)
+              (list 'number 'one 'with 'us))))
