c7055f071074e92af46d771ae73e62c135ce9177
josh-reynolds <josh.reynolds@frontier.com>
2024-01-25 13:28:34 -0800

    Chapter 10: What is the Value of All of This? - through p. 176

--- Chapter_10/definitions.rkt
+++ Chapter_10/definitions.rkt
@@ -22,8 +22,36 @@
 
 ; ------------------------------
 (define lookup-in-entry
-  (lambda (name entry)
-    (...)))
+  (lambda (name entry entry-f)
+    (lookup-in-entry-help name
+                          (first entry)
+                          (second entry)
+                          entry-f)))
+; ------------------------------
+
+; ------------------------------
+(define lookup-in-entry-help
+  (lambda (name names values entry-f)
+    (cond
+      ((null? names) (entry-f name))
+      ((eq? name (car names)) (car values))
+      (else (lookup-in-entry-help name (cdr names) (cdr values) entry-f)))))
+; ------------------------------
+
+; ------------------------------
+(define first
+  (lambda (l)
+    (car l)))
+; ------------------------------
+
+; ------------------------------
+(define second
+  (lambda (l)
+    (car (cdr l))))
+; ------------------------------
+
+; ------------------------------
+(define extend-table cons)
 ; ------------------------------
 
 
@@ -39,3 +67,13 @@
   (list (list 'beverage 'dessert)
         (list (list 'food 'is)
               (list 'number 'one 'with 'us))))
+
+(define table1
+  (list entry1
+        entry3))
+
+(define table2
+  (list (list (list 'entrée 'dessert)
+              (list 'spaghetti 'spumoni))
+        (list (list 'appetizer 'entrée 'beverage)
+              (list 'food 'tastes 'good))))
